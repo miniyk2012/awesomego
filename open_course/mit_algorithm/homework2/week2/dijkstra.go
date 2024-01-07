1ba2253488d6d39c92bd9ffbfade2e8f5e9e1417
yangkai.04 <yangkai.04@bytedance.com>
2024-01-07 18:21:16 +0800

    [feat] naive sp

--- open_course/mit_algorithm/homework2/week2/dijkstra.go
+++ open_course/mit_algorithm/homework2/week2/dijkstra.go
@@ -11,30 +11,7 @@ import (
 	"strings"
 )
 
-// 虽然作业里是无向图, 但为了通用性, 我还是创建一个有向图, 并且提供个方法判断该图任意2点间必然有对称的边, 以证明测试数据是无向图数据.
-
-type WeightedDiEdge struct {
-	v      int // from
-	w      int // to
-	weight int // 权重
-}
-
-func (e WeightedDiEdge) String() string {
-	return fmt.Sprintf("%d->%d: %d", e.v+1, e.w+1, e.weight)
-}
-
-type WeightedDiGraph struct {
-	V   int // 点个数
-	E   int // 边条数
-	adj []*list.List
-}
-
-type Iterator interface {
-	HasNext() bool
-	Next() WeightedDiEdge
-	// Rewind 重新回到迭代器的起点，即第一个数据
-	Rewind()
-}
+const INIT_DISTANCE = 1_000_000
 
 type EdgeIterator struct {
 	l      *list.List
@@ -57,6 +34,31 @@ func (it *EdgeIterator) Next() WeightedDiEdge {
 
 func (it *EdgeIterator) Rewind() {
 	it.curEle = it.l.Front()
+}
+
+// WeightedDiEdge 边
+type WeightedDiEdge struct {
+	v      int // from
+	w      int // to
+	weight int // 权重
+}
+
+func (e WeightedDiEdge) String() string {
+	return fmt.Sprintf("%d->%d: %d", e.v+1, e.w+1, e.weight)
+}
+
+// WeightedDiGraph 虽然作业里是无向图, 但为了通用性, 我还是创建一个有向图, 并且提供个方法判断该图任意2点间必然有对称的边, 以证明测试数据是无向图数据.
+type WeightedDiGraph struct {
+	V   int // 点个数
+	E   int // 边条数
+	adj []*list.List
+}
+
+type Iterator interface {
+	HasNext() bool
+	Next() WeightedDiEdge
+	// Rewind 重新回到迭代器的起点，即第一个数据
+	Rewind()
 }
 
 func NewWeightedDiGraph(V int) *WeightedDiGraph {
@@ -160,10 +162,65 @@ func LoadGraph(filePath string) *WeightedDiGraph {
 	return graph
 }
 
-func NaiveDijkstra() {
-
+type NaiveDijkstra struct {
+	graph  *WeightedDiGraph
+	source int          // source(下标0~V-1)
+	X      map[int]bool // 已算出来的节点(key是0~V-1)
+	A      []int        // A[i] = 100 表示source到节点i(下标0~V-1)的最短距离是100
+	B      []*list.List //  B[i]是source到i的一条最短路径
 }
 
-func HeadBasedDijkstra() {
+func InitNaiveDijkstra(graph *WeightedDiGraph, s int) *NaiveDijkstra {
+	x := make(map[int]bool)
+	x[s-1] = true
+	a := make([]int, graph.V)
+	a[s-1] = 0
+	b := make([]*list.List, graph.V)
+	for i := range b {
+		b[i] = list.New()
+	}
+	return &NaiveDijkstra{
+		graph:  graph,
+		source: s - 1,
+		X:      x,
+		A:      a,
+		B:      b,
+	}
+}
 
+func (n *NaiveDijkstra) Shortest(w int) int {
+	return n.A[w]
+}
+func (n *NaiveDijkstra) Run() {
+	for len(n.X) < n.graph.V {
+		n.findNewW()
+	}
+}
+
+func (n *NaiveDijkstra) findNewW() {
+	var shortestDistance = INIT_DISTANCE
+	var selectedEdge WeightedDiEdge
+	for v := range n.X {
+		it := n.graph.Adj(v)
+		for it.HasNext() {
+			edge := it.Next()
+			if _, ok := n.X[edge.w]; !ok {
+				if n.A[v]+edge.weight < shortestDistance {
+					shortestDistance = n.A[v] + edge.weight
+					selectedEdge = edge
+				}
+			}
+		}
+	}
+	n.A[selectedEdge.w] = shortestDistance
+	n.X[selectedEdge.w] = true
+	l := list.New()
+	l.PushBackList(n.B[selectedEdge.v])
+	l.PushBack(selectedEdge)
+	n.B[selectedEdge.w] = l
+}
+
+type HeapBasedDijkstra struct {
+	graph  *WeightedDiGraph
+	source int // source(下标0~V-1)
 }
--- open_course/mit_algorithm/homework2/week2/dijkstra_test.go
+++ open_course/mit_algorithm/homework2/week2/dijkstra_test.go
@@ -1,6 +1,10 @@
 package week2
 
 import (
+	"fmt"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,14 +13,17 @@ import (
 func TestLoadGraph(t *testing.T) {
 	filePath := "./dijkstraData.txt"
 	graph := LoadGraph(filePath)
+	assert.NotNil(t, graph)
 	assert.True(t, graph.IsSymmetry())
 	it := graph.Adj(1) // 2的出边
 	for it.HasNext() {
 		t.Log(it.Next())
 	}
-	t.Log()
+	t.Logf("dijkstraData Graph V=%d, E=%d", graph.V, graph.V)
+
 	filePath = "./testdata/testcase1.txt"
 	graph = LoadGraph(filePath)
+	assert.NotNil(t, graph)
 	assert.True(t, graph.IsSymmetry())
 	allEdges := graph.Edges()
 	var totalEdges int
@@ -25,4 +32,63 @@ func TestLoadGraph(t *testing.T) {
 		t.Log(allEdges.Next())
 	}
 	assert.Equal(t, 16, totalEdges)
+	assert.Equal(t, 16, graph.E)
+	assert.Equal(t, 8, graph.V)
+}
+
+var testcases = []struct {
+	testcaseFile string
+	dist         []int
+	paths        [][]int
+}{
+	{"testcase1.txt", []int{0, 1, 2, 3, 4, 4, 3, 2}, [][]int{
+		nil, {2}, {2, 3}, {2, 3, 4}, {2, 3, 4, 5}, {8, 7, 6}, {8, 7}, {8},
+	}},
+}
+
+func TestNaiveSP(t *testing.T) {
+	for _, testcase := range testcases {
+		t.Run(testcase.testcaseFile, func(t *testing.T) {
+			graph := LoadGraph(filepath.Join("./testdata", testcase.testcaseFile))
+			sp := InitNaiveDijkstra(graph, 1)
+			sp.Run()
+			for i := 0; i < graph.V; i++ {
+				var sb strings.Builder
+				var path []int
+				it := NewEdgeIterator(sp.B[i])
+				for it.HasNext() {
+					edge := it.Next()
+					sb.WriteString(fmt.Sprintf("%s, ", edge))
+					path = append(path, edge.w+1)
+				}
+				line := sb.String()
+				sb.Reset()
+				if len(line) >= 2 {
+					sb.WriteString(line[:len(line)-2])
+					sb.WriteString(": ")
+				}
+				sb.WriteString(fmt.Sprintf("1 to %d distance=%d", i+1, sp.Shortest(i)))
+				t.Log(sb.String())
+				assert.Equal(t, testcase.dist[i], sp.Shortest(i))
+				assert.Equal(t, testcase.paths[i], path)
+			}
+		})
+	}
+}
+
+func TestAssignment(t *testing.T) {
+	var nodes = []int{7, 37, 59, 82, 99, 115, 133, 165, 188, 197}
+	graph := LoadGraph("./dijkstraData.txt")
+	assert.NotNil(t, graph)
+	sp := InitNaiveDijkstra(graph, 1)
+	sp.Run()
+	var sb strings.Builder
+	for i, id := range nodes {
+		v := sp.Shortest(id - 1)
+		sb.WriteString(strconv.Itoa(v))
+		if i < len(nodes)-1 {
+			sb.WriteString(",")
+		}
+	}
+	assert.Equal(t, "2599,2610,2947,2052,2367,2399,2029,2442,2505,3068", sb.String())
 }
