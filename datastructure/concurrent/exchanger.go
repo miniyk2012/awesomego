f7ed507509b3c64db786988b882dc40d3db2740e
yangkai.04 <yangkai.04@bytedance.com>
2024-01-20 09:32:54 +0800

    [feat] exchanger test

--- datastructure/concurrent/exchanger.go
+++ datastructure/concurrent/exchanger.go
@@ -1,1 +1,49 @@
 package concurrent
+
+import (
+	"sync/atomic"
+
+	"github.com/smallnest/goroutine"
+)
+
+type Exchanger[T any] struct {
+	leftGoID, rightGoID int64
+	left, right         chan T
+}
+
+// NewExchanger creates a new exchanger.
+func NewExchanger[T any]() *Exchanger[T] {
+	return &Exchanger[T]{
+		leftGoID:  -1,
+		rightGoID: -1,
+		left:      make(chan T, 1),
+		right:     make(chan T, 1),
+	}
+}
+
+func (e *Exchanger[T]) Exchange(value T) T {
+	goid := goroutine.ID()
+
+	// left goroutine
+	isLeft := atomic.CompareAndSwapInt64(&e.leftGoID, -1, goid)
+	if !isLeft {
+		isLeft = atomic.LoadInt64(&e.leftGoID) == goid
+	}
+	if isLeft {
+		e.right <- value // send value to right
+		return <-e.left  // wait for value from right
+	}
+
+	// right goroutine
+	isRight := atomic.CompareAndSwapInt64(&e.rightGoID, -1, goid)
+	if !isRight {
+		isRight = atomic.LoadInt64(&e.rightGoID) == goid
+	}
+	if isRight {
+		e.left <- value  // send value to left
+		return <-e.right // wait for value from left
+	}
+
+	// other goroutine
+	panic("sync: exchange called from neither left nor right goroutine")
+}
--- datastructure/concurrent/exchanger_test.go
+++ datastructure/concurrent/exchanger_test.go
@@ -1,11 +1,16 @@
-package concurrent
+package concurrent_test
 
 import (
 	"bytes"
+	"fmt"
+	"sync"
 	"testing"
+
+	"github.com/miniyk2012/awesomego/datastructure/concurrent"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestExchanger(t *testing.T) {
+func TestWriteBytes(t *testing.T) {
 	buf := bytes.NewBuffer(make([]byte, 1024))
 	expect := 0
 	for j := 0; j < 1024; j++ {
@@ -13,4 +18,53 @@ func TestExchanger(t *testing.T) {
 		expect += j / 256
 	}
 	t.Logf("expect=%d", expect)
+	assert.Equal(t, 1536, expect)
+}
+
+func ExampleExchanger() {
+	buf1 := bytes.NewBuffer(make([]byte, 1024))
+	buf2 := bytes.NewBuffer(make([]byte, 1024))
+
+	exchanger := concurrent.NewExchanger[*bytes.Buffer]()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	expect := 0
+	go func() {
+		defer wg.Done()
+
+		buf := buf1
+		for i := 0; i < 10; i++ {
+			for j := 0; j < 1024; j++ {
+				buf.WriteByte(byte(j / 256))
+				expect += j / 256
+			}
+
+			buf = exchanger.Exchange(buf)
+		}
+	}()
+
+	var got int
+	go func() {
+		defer wg.Done()
+
+		buf := buf2
+		for i := 0; i < 10; i++ {
+			buf = exchanger.Exchange(buf)
+			for _, b := range buf.Bytes() {
+				got += int(b)
+			}
+			buf.Reset()
+		}
+	}()
+
+	wg.Wait()
+
+	fmt.Println(got)
+	fmt.Println(expect == got)
+
+	// Output:
+	// 15360
+	// true
 }
