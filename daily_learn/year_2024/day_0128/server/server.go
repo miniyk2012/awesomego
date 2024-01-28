45c74f2ecfd316715b9319a4032e2c06208cea29
yangkai.04 <yangkai.04@bytedance.com>
2024-01-28 18:33:29 +0800

    [feat] rpc go

---
+++ daily_learn/year_2024/day_0128/server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type Args struct {
+	X, Y int
+}
+
+// ServiceA 自定义一个结构体类型
+type ServiceA struct{}
+
+// Add 为ServiceA类型增加一个可导出的Add方法
+func (s *ServiceA) Add(args *Args, reply *int) error {
+	*reply = args.X + args.Y
+	return nil
+}
+
+func main() {
+	service := new(ServiceA)
+	rpc.Register(service) // 注册RPC服务
+	l, e := net.Listen("tcp", ":9091")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	for {
+		conn, _ := l.Accept()
+		rpc.ServeConn(conn)
+	}
+}
---
+++ datastructure/concurrent/cyclicbarrier_test.go
@@ -0,0 +1,5 @@
+package concurrent
+
+func main() {
+	
+}
