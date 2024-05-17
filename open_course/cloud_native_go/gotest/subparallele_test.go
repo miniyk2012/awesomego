eb520fbb8b5f3ea238ec35f1fdf054d36c8db923
yangkai.04 <yangkai.04@bytedance.com>
2024-05-17 12:53:42 +0800

    [feat] unittest 1

---
+++ open_course/cloud_native_go/gotest/subparallele_test.go
@@ -0,0 +1,54 @@
+package gotest_test
+
+import (
+	"testing"
+	"time"
+)
+
+func parallelSub1(t *testing.T) {
+	t.Parallel()
+	t.Logf("test1")
+	time.Sleep(3 * time.Second)
+}
+
+func parallelSub2(t *testing.T) {
+	t.Parallel()
+	t.Logf("test2")
+	time.Sleep(2 * time.Second)
+}
+
+func parallelSub3(t *testing.T) {
+	t.Parallel()
+	t.Logf("test3")
+	time.Sleep(1 * time.Second)
+}
+
+func TestSubParallel(t *testing.T) {
+	t.Logf("set up")
+	t.Run("group", func(t *testing.T) {
+		t.Run("Test1", parallelSub1)
+		t.Run("Test2", parallelSub2)
+		t.Run("Test3", parallelSub3)
+		t.Logf("inner teardown")
+	})
+	t.Logf("tear down")
+}
+
+func TestSubParallel2(t *testing.T) {
+	t.Logf("set up")
+	t.Cleanup(func() {
+		t.Logf("tear down")
+	})
+	t.Run("Test1", parallelSub1)
+	t.Run("Test2", parallelSub2)
+	t.Run("Test3", parallelSub3)
+	t.Logf("inner teardown")
+}
+
+func TestSubParallel3(t *testing.T) {
+	t.Logf("set up")
+	t.Run("Test1", parallelSub1)
+	t.Run("Test2", parallelSub2)
+	t.Run("Test3", parallelSub3)
+	t.Logf("tear down")
+}
